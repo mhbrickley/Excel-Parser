3055267bcf705a008dff410bcb9106565f8c250f
Michael <michael.brickley1@gmail.com>
2019-03-01 12:03:06 -0500

    Update excelParser.py

--- excelParser.py
+++ excelParser.py
@@ -1,6 +1,8 @@
 '''
-Excel Parser
-This program parses tabs within an Excel workbook into separate files
+Author: Michael Brickley
+Python Version [2/3]: 3
+Title: Excel Parser
+Description: This program parses tabs within an Excel workbook into separate files,
 based on user defined file extension and delimiter
 '''
 
